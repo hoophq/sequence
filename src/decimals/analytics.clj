82e0ad9d4abf1118de826ee58af53b376467813f
Andrios <andriosr@gmail.com>
2020-09-08 22:40:25 -0300

    fix(analytics) add checks for optional analytics events off mode

--- src/decimals/analytics.clj
+++ src/decimals/analytics.clj
@@ -6,12 +6,16 @@
 
 (defstate analytics :start (try
                              (a/initialize (env :segment-key))
-                             (catch Exception e (log/warnf "failed to initialize analytics: %s" (.getMessage e)))))
+                             (catch Exception e
+                               (log/warnf "failed to initialize analytics: %s"
+                                          (.getMessage e)))))
 
 (defn track [context event traits]
-  (if-let [email (get-in context [:customer :email])]
-    (a/track analytics email event traits)))
+  (when-let [analytics analytics]
+    (when-let [email (get-in context [:customer :email])]
+      (a/track analytics email event traits))))
 
 (defn identify [context]
-  (when-let [email (get-in context [:customer :email])]
-    (a/identify analytics email {:email email})))
+  (when-let [analytics analytics]
+    (when-let [email (get-in context [:customer :email])]
+      (a/identify analytics email {:email email}))))
