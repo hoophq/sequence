634fe407042361b88e8b2238d65069080a8885db
Andrios <andriosr@gmail.com>
2020-09-12 21:58:23 -0300

    fix(crypto) fix map sorting by keys logic

--- src/decimals/crypto.clj
+++ src/decimals/crypto.clj
@@ -13,10 +13,14 @@
        (BigInteger. 1)
        (format "%032x")))
 
+(defn sort-map [data]
+  (into (sorted-map) (sort-by first (seq data))))
+
 (defn map->md5
   [data]
   (->> data
-       sorted-map
+       sort-map
+       prn-str
        .getBytes
        (.digest (MessageDigest/getInstance "MD5"))
        (BigInteger. 1)
